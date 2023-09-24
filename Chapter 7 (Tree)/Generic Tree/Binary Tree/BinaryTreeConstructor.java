6a09f1094b42e9a1f2bd92fb1c3ec19ceacedd2e
nikkipanchal <nikki@revinfotech.com>
2023-09-24 09:16:03 +0530

    started binnary tree

---
+++ Chapter 7 (Tree)/Generic Tree/Binary Tree/BinaryTreeConstructor.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class BinaryTreeConstructor {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
